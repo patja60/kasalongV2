3ed9f001744c2bd214c4958e5a548a56e3acc79e
Thanapat Juthavantana <patja60@outlook.co.th>
2018-08-26 13:14:41 +0700

    add createStore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
