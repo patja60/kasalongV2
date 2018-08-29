e9cc1b139e99673cee88b4f6fe2afa789161c867
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2018-08-29 17:14:52 +0700

    added proptypes checking and renamed AuthActions.js to authActions.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import {
   emailChanged,
@@ -92,6 +93,12 @@ class Login extends Component {
       </div>
     );
   }
+}
+
+Login.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string,
+  user: PropTypes.object
 }
 
 const mapStateToProps = state => {
