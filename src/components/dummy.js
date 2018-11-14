174d340b8f252bd0b944d7ae0baac494d21ffa4e
Thanapat Juthavantana <patja60@outlook.co.th>
2018-11-14 20:00:16 +0700

    I update new dummy data for subject

--- src/components/dummy.js
+++ src/components/dummy.js
@@ -12,3 +12,34 @@ export default [
     roomNo: 4
   }
 ];
+
+// new dummy subject should look like This
+
+// [{
+//     subId: 1,
+//     subName: "Exploring Engineering World",
+//     sec: [{
+//       secNumber: 1,
+//       subTime: 2,
+//       capacity: 40,
+//       current: 2,
+//       studentList: [{name: "Patja", timeStamp: "..."},{name: "fudgy", timeStamp: "..."}]
+//     },{
+//       secNumber: 2,
+//       subTime: 5,
+//       capacity: 40,
+//       current: 0,
+//       studentList: [{}]
+//     }]
+//   },
+//   {
+//     subId: 1,
+//     subName: "Exploring Engineering World",
+//     sec: [{
+//       secNumber: 1,
+//       subTime: 2,
+//       capacity: 40,
+//       current: 2,
+//       studentList: [{name: "Patja", timeStamp: "..."},{name: "fudgy", timeStamp: "..."}]
+//     }]
+//   }];
