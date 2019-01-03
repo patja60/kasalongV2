37b49ffde1061fbfb486ddc62f0ac67933e60ef6
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2019-01-03 12:09:20 +0700

    fixed dummy and timetable

--- src/components/dummy.js
+++ src/components/dummy.js
@@ -1,47 +1,24 @@
 export default [
   {
-    subId: 1,
-    subName: "Exploring Engineering World",
+    subjectId: 1,
+    subjectName: "Exploring Engineering World",
     subjectPassword: "123456",
-    sec: {
-      0: {
-        //There is always atlest 1 sec.
-        sec: 1,
-        subjectTime: 2,
-        subjectTimeMap: 2,
-        capacity: 40,
-        currentStudent: 2,
-        studentList: {
-          userId: { name: "Patja", timeStamp: "..." },
-          userId: { name: "fudgy", timeStamp: "..." }
-        }
-      },
-      1: {
-        sec: 2,
-        subjectTime: 3,
-        subjectTimeMap: "1_2",
-        capacity: 40,
-        currentStudent: 0,
-        studentList: null
-        // this studentList node can be null if there is no student register yet, be careful.
-      }
-    }
+    sec: 1,
+    subjectTime: 2,
+    subjectTimeMap: "2"
   },
   {
-    subId: 2,
-    subName: "Express Bussiness",
-    sec: {
-      1: {
-        sec: 1,
-        subjectTime: 5,
-        subjectTimeMap: "1_4",
-        capacity: 40,
-        currentStudent: 2,
-        studentList: {
-          userId: { name: "Patja", timeStamp: "..." },
-          userId: { name: "fudgy", timeStamp: "..." }
-        }
-      }
-    }
+    subjectId: 2,
+    subjectName: "Express Bussiness",
+    sec: 1,
+    subjectTime: 5,
+    subjectTimeMap: "1_4"
+  },
+  {
+    subjectId: 3,
+    subjectName: "Invest to the future",
+    sec: 1,
+    subjectTime: 13,
+    subjectTimeMap: "32_1024"
   }
 ];
